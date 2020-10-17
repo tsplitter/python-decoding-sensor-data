8393a25e1e247a06aba098902b0c26bc9bdc8638
tsplitter <pa.tsplitter@yahoo.com>
2020-10-17 06:08:34 +0100

    Completed module

--- sensor/particle_count_info.py
+++ sensor/particle_count_info.py
@@ -1,5 +1,5 @@
 from house_info import HouseInfo
-
+from datetime import date
 class ParticleData(HouseInfo):
     def _convert_data(self, data):
         recs = []
--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,9 +1,12 @@
+
 # Runner script for all modules
-from humidity_info import HumidityData
-from datetime import date, datetime
 from load_data import load_sensor_data
 from house_info import HouseInfo
+from datetime import datetime, date
 from temperature_info import TemperatureData
+from humidity_info import HumidityData
+from statistics import mean
+from particle_count_info import ParticleData
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
@@ -15,16 +18,6 @@ print("Sensor Data App")
 data = load_sensor_data()
 print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
-
-# Module 3 code here:
-
-# Module 4 code here:
-
-# Module 5 code here:i
-
-
-
-
 house_info = HouseInfo(data)
 test_area = 1
 recs = house_info.get_data_by_area("id", rec_area=test_area)
@@ -32,27 +25,20 @@ print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
 recs = house_info.get_data_by_date("id", rec_date=test_date)
-print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+print("House sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
+# Module 3 code here:
 temperature_data = TemperatureData(data)
-
 recs = temperature_data.get_data_by_area(rec_area=test_area)
 print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 
 recs = temperature_data.get_data_by_date(rec_date=test_date)
-print("\nHouse Temperature sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
+print("\nHouse Temperature sensor records for date: {} = {}".format(
+    test_date.strftime("%m/%d/%y"), len(recs)))
 print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 
-himidity_data = HumidityData(data)
-recs = humidity_data.get_data_by_area(rec_area=test_area)
-print("\nHouse Humidity sensor records for area {} = {}".format(test_area, len(recs)))
-print("\tAverage: {} humidity".format(mean(recs)))
-
-recs = humidity.get_data_by_date(rec_date=test_date)
-print("House Humidity sensor records for date: {} = {}".format( test_date.strftime("%m/%d/%y"), len(recs)))
-print("\tAverrage: {} humidity".format(mean(recs)))
-
+# Module 4 code here:
 humidity_data = HumidityData(data)
 recs = humidity_data.get_data_by_area(rec_area=test_area)
 print("\nHouse Humidity sensor records for area {} = {}".format(test_area, len(recs)))
@@ -63,7 +49,7 @@ print("House Humidity sensor records for date: {} = {}".format(
     test_date.strftime("%m/%d/%y"), len(recs)))
 print("\tAverrage: {} humidity".format(mean(recs)))
 
-
+# Module 4B
 particle_data = ParticleData(data)
 recs = particle_data.get_data_by_area(rec_area=test_area)
 print("\nHouse Particle sensor records for area {} = {}".format(test_area, len(recs)))
@@ -72,12 +58,12 @@ print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 
-
+recs = particle_data.get_data_by_date(rec_date=test_date)
 print("\nHouse Particle sensor records for date: {} = {}".format(
     test_date.strftime("%m/%d/%y"), len(recs)))
 concentrations = particle_data.get_data_concentrations(data = recs)
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
-i
+# Module 5 code here:
 
